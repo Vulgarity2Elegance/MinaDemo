3d81561bb7e583877d90a1ccc1902259415eacf0
Youzhi Hu <jyzhu0101@gmail.com>
2021-02-11 17:25:27 +1100

    Get data for swiperList and Categlogue.

---
+++ pages/index/index.js
@@ -0,0 +1,48 @@
+// 引入文件 路径补全
+import {request} from "../../request/index.js"
+//Page Object
+Page({
+  data: {
+    //轮播图数组
+    swiperList:[],
+    //导航数组
+    cataList:[]
+  },
+  //页面开始加载就会触发
+  onLoad: function(options) {
+    this.getSwiperList()
+    this.getCataList()
+  },
+
+  //获取轮播图数据
+  async getSwiperList(){
+    // 1. 发送异步请求 
+    // wx.request({
+    //   url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata',
+    //   success: (result) => {
+    //     this.setData({swiperList:result.data.message})
+    //   }
+    // });
+    // 2. 用promise来优化异步请求
+    // request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+    // .then(result => {
+    //   this.setData({
+    //     swiperList: result.data.message
+    //   })
+    // })
+    // 3. async/await syntax sugar 
+    const result = await request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/swiperdata'})
+    this.setData({
+      swiperList: result.data.message
+    })
+  },
+
+  //获取分类导航数据
+  async getCataList(){
+    const result = await request({url: 'https://api-hmugo-web.itheima.net/api/public/v1/home/catitems'})
+    this.setData({
+      cataList: result.data.message
+    })
+  }
+});
+  
